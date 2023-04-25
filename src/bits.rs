b3f799a60a85a5266f99da05c91edb14043401d4
52617365 <rasengithub@protonmail.com>
2023-04-25 19:21:24 +0300

    added new masks and changed some existing ones.

--- src/bits.rs
+++ src/bits.rs
@@ -1,3 +1,4 @@
+use crate::operation::*;
 use bitflags::bitflags;
 bitflags! {
     #[derive(PartialEq, Eq)]
@@ -26,7 +27,7 @@ bitflags! {
     pub struct MEMORY_TO_REGISTER_VICA_VERCA_MNEMONIC_MASK_RESULTS: u8{
        // TODO: there is some other alternative we have to add here. Figure out what it is.
        // used on the first byte.
-       const MOV = 0b_10001000;
+       const MOV = 0b_00001000;
        const ADD = 0b_00000000;
        const SUB = 0b_00101000;
        const CMP = 0b_00111000;
@@ -50,7 +51,7 @@ bitflags! {
         const REGISTER_MODE = 0b_11_000_000;
         const MEMORY_MODE_8 = 0b_01_000_000;
         const MEMORY_MODE_16 = 0b_10_000_000;
-        const MEMORY_MODE = 0b_00_000_000; // (Only if r/m is not 110, then its a 16 bit placement).
+        const MEMORY_MODE = 0b_00_000_000; // (No displacement when r/m is not 110, else its a 16 bit placement).
     }
 
     #[derive(PartialEq, Eq)]
@@ -94,7 +95,7 @@ bitflags! {
         const IMMEDIATE_TO_REGISTER_W_MASK = 0b_0000_1000; // This is the W bit of a memory to register, register to memory and register to register move and it's responsible for determining the size of the registers (8 or 16 bit).
         const MEMORY_TO_REGISTER_VICA_VERCA_W_MASK = 0b000000_01; // This is the W bit of a memory to register, register to memory and register to register move and it's responsible for determining the size of the registers (8 or 16 bit).
         const IMMEDIATE_TO_REGISTER_REG_FIELD_MASK = 0b_00_000_111; // this is used to get the contents of the REG field if it's present in the first byte.
-        const MEMORY_TO_REGISTER_VICA_VERCA_MNEMONIC_MASK = 0b_11111100; // To determine what mnemonic is being used.
+        const MEMORY_TO_REGISTER_VICA_VERCA_MNEMONIC_MASK = 0b_00111000; // To determine what mnemonic is being used.
         const IMMEDIATE_TO_REGISTER_OR_MEMORY_W_BIT = 0b_00000001; // This instruction actually relies on the second byte also since its mod field determines which mnemonic is being used.
         const IMMEDIATE_TO_REGISTER_OR_MEMORY_S_BIT = 0b_00000010; // If this (S bit) is set to 1, and W = 1 then
     }
@@ -105,4 +106,106 @@ bitflags! {
         const RM_MASK = 0b_00_000_111; // this is used to get the contents of the R/M field
     }
 
+    pub struct MOD_RESULTS: u8 {
+        const REGISTER_MODE = 0b_11_000_000;
+        const MEMORY_MODE = 0b_00_000_000; // No displacement follows expect 16-bit when r/m = 110
+        const MEMORY_MODE_8_BIT_DISPLACEMENT = 0b_01_000_000;
+        const MEMORY_MODE_16_BIT_DISPLACEMENT = 0b_10_000_000;
+    }
+
+    #[derive(PartialEq)]
+    pub struct REG_OR_MEMORY_WITH_REGISTER_MNEMONIC_RESULTS: u8 {
+        const MOV = 0b_10001000;
+        const ADD = 0b_00000000;
+        const SUB = 0b_00101000;
+        const CMP = 0b_00111000;
+    }
+
+    #[derive(PartialEq)]
+    pub struct IMMEDIATE_TO_MEMORY_OR_REGISTER: u8 {
+        const MOV = 0b_1100_0100;
+
+        // SUB and CMP are the same in the first byte, the second fields REG field determines the difference.
+        const SUB_OR_CMP_OR_ADD = 0b_1000_0000;
+    }
+
+
+    pub struct MASKS: u8 {
+        const MOD = 0b_11_000_000;
+
+        // r/m is basically always in the same bits.
+        const RM = 0b_00_000_111;
+
+        // REG is usually in the same place. There are some edge cases however for example the mov immediate_to_register.
+        const REG = 0b_00_111_000;
+
+        const MOV_IMMEDIATE_TO_REGISTER_W_BIT = 0b_00_001_000; // there is one exception i saw with the w bit and thats mov immediate to register.
+
+        const IMMEDIATE_TO_MEMORY_OR_REGISTER_S_BIT = 0b_00_000_010; // TODO: what is this exactly for?
+
+        const W_BIT = 0b_00_000_001; // this is used for basically everything.
+
+        //
+        const MOV_IMMEDIATE_TO_REGISTER = 0b_10110000; // determines the mov immediate to register operation.
+
+        const REG_OR_MEMORY_WITH_REGISTER = 0b_10111000; // determines if it's a reg/memory and register to either operation.
+
+        const IMMEDIATE_TO_MEMORY_OR_REGISTER = 0b_11111100; // determines if the operation is an immediate to register or immediate to memory operation.
+    }
+
+}
+
+impl REG_OR_MEMORY_WITH_REGISTER_MNEMONIC_RESULTS {
+    pub fn convert_to_mnemonic(res: u8) -> &'static str {
+        if res == REG_OR_MEMORY_WITH_REGISTER_MNEMONIC_RESULTS::MOV.bits() {
+            return "mov";
+        } else if res == REG_OR_MEMORY_WITH_REGISTER_MNEMONIC_RESULTS::ADD.bits() {
+            return "add";
+        } else if res == REG_OR_MEMORY_WITH_REGISTER_MNEMONIC_RESULTS::SUB.bits() {
+            return "sub";
+        } else if res == REG_OR_MEMORY_WITH_REGISTER_MNEMONIC_RESULTS::CMP.bits() {
+            return "cmp";
+        } else {
+            panic!("unknown bit pattern: {:08b}.", res)
+        }
+    }
+}
+
+impl IMMEDIATE_TO_MEMORY_OR_REGISTER {
+    // the IMMEDIATE_TO_MEMORY_OR_REGISTER convert to mnemonic function relies on the second bytes reg
+    // to determine the difference between sub and cmp
+    pub fn convert_to_mnemonic(res: u8, second_byte: u8) -> &'static str {
+        if res == IMMEDIATE_TO_MEMORY_OR_REGISTER::MOV.bits() {
+            return "mov";
+        } else if res == IMMEDIATE_TO_MEMORY_OR_REGISTER::SUB_OR_CMP_OR_ADD.bits() {
+            let REG = second_byte & MASKS::REG.bits();
+            if REG == 0b_00_000_000 {
+                return "add";
+            } else if REG == 0b_00_101_000 {
+                return "sub";
+            } else if REG == 0b_00_111_000 {
+                return "cmp";
+            } else {
+                panic!("Unsupported reg result: {:08b}", REG)
+            }
+        } else {
+            panic!("unknown bit pattern: {:08b}.", res)
+        }
+    }
+}
+
+impl MOD_RESULTS {
+    pub fn convert_to_operation(r: u8) -> Operation {
+        if r == MOD_RESULTS::MEMORY_MODE.bits() {
+            return Operation::MEMORY_MODE_NONE;
+        } else if r == MOD_RESULTS::MEMORY_MODE_8_BIT_DISPLACEMENT.bits() {
+            return Operation::MEMORY_MODE_8_BIT_DISPLACEMENT;
+        } else if r == MOD_RESULTS::MEMORY_MODE_16_BIT_DISPLACEMENT.bits() {
+            return Operation::MEMORY_MODE_16_BIT_DISPLACEMENT;
+        } else if r == MOD_RESULTS::REGISTER_MODE.bits() {
+            return Operation::REGISTER_MODE;
+        } else {
+            panic!("Unsupported operation: {:08b}", r)
+        }
+    }
 }
